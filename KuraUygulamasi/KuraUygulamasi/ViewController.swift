ae8cf7ba422a8be072f50f4f981138b8c13d06eb
Eyup KORURER <ekorurer@innova.com.tr>
2021-09-26 15:24:03 +0300

    Initial commit

---
+++ KuraUygulamasi/KuraUygulamasi/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  KuraUygulamasi
+//
+//  Created by Eyup KORURER on 26.09.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
